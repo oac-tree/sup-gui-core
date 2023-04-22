1377d2381b65593aaaa898b748219d844a8cf054
Rafael Graça Silva <rg-silva@criticalsoftware.com>
2023-04-22 05:11:19 +0100

    sup/gui/widgets: implement generic AboutApplicationDialog
    Dialog displaying basic info about application, configurable to be used accross projects

---
+++ src/lib/sup-gui-core/sup/gui/widgets/about_application_dialog.cpp
@@ -0,0 +1,137 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Rafael Siva (EXT)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "about_application_dialog.h"
+
+#include <mvvm/widgets/widget_utils.h>
+
+#include <QDate>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+namespace sup::gui
+{
+
+AboutApplicationDialog::AboutApplicationDialog(QWidget* parent, const QString& app_name,
+                                               const QString& description, const QString& version)
+    : QDialog(parent)
+{
+  setWindowTitle("About " + app_name);
+
+  auto details_layout = new QHBoxLayout;
+  details_layout->addLayout(CreateLogoLayout());
+  details_layout->addLayout(CreateTextLayout(app_name, description, version));
+
+  auto main_layout = new QVBoxLayout;
+  main_layout->addLayout(details_layout);
+  main_layout->addLayout(CreateButtonLayout());
+
+  setLayout(main_layout);
+}
+
+void AboutApplicationDialog::ShowDialog(QWidget* parent, const QString& app_name,
+                                        const QString& description, const QString& version)
+{
+  AboutApplicationDialog dialog(parent, app_name, description, version);
+  dialog.exec();
+}
+
+QString AboutApplicationDialog::GetCopyRight()
+{
+  QDate date = QDate::currentDate();
+  return QString("Copyright: 2010-%1 ITER Organization ").arg(date.toString("yyyy"));
+}
+
+QLabel* AboutApplicationDialog::CreateLinkLabel(const QString& link, const QString& name)
+{
+  auto link_label = new QLabel();
+  link_label->setTextFormat(Qt::RichText);
+  link_label->setTextInteractionFlags(Qt::TextBrowserInteraction);
+  link_label->setOpenExternalLinks(true);
+  link_label->setText(QString("<a href=\"%1\">%2</a>").arg(link, name));
+  return link_label;
+}
+
+QLayout* AboutApplicationDialog::CreateLogoLayout()
+{
+  auto layout = new QVBoxLayout;
+  auto label = new QLabel;
+
+  label->setPixmap(
+      QPixmap(":/icons/iter_logo.png")
+          .scaled(mvvm::utils::UnitSize(10), mvvm::utils::UnitSize(10), Qt::KeepAspectRatio));
+
+  layout->addWidget(label);
+  layout->addStretch(1);
+
+  const auto gap = mvvm::utils::UnitSize(1.0);
+  layout->setContentsMargins(0, gap, gap, gap);
+
+  return layout;
+}
+
+QLayout* AboutApplicationDialog::CreateTextLayout(const QString& app_name,
+                                                  const QString& description,
+                                                  const QString& version)
+{
+  auto layout = new QVBoxLayout;
+
+  // Title
+  auto about_title_label = new QLabel(app_name + " version " + version);
+  mvvm::utils::ScaleLabelFont(about_title_label, 1.2, true);
+  about_title_label->setContentsMargins(0, 0, 0, mvvm::utils::UnitSize());
+
+  // Copyright
+  auto copyright_label = new QLabel(GetCopyRight());
+
+  // Description
+  auto description_label = new QLabel(description);
+  description_label->setWordWrap(true);
+
+  layout->addWidget(about_title_label);
+  layout->addWidget(description_label);
+  layout->addStretch(1);
+
+  // Copyright
+  layout->addWidget(copyright_label);
+  layout->addWidget(
+      CreateLinkLabel("https://git.iter.org/projects/COA/repos", "CODAC Operational Applications"));
+  layout->addStretch(1);
+
+  auto gap = mvvm::utils::UnitSize(1.0);
+  layout->setContentsMargins(0, gap, gap, gap);
+
+  return layout;
+}
+
+QLayout* AboutApplicationDialog::CreateButtonLayout()
+{
+  auto layout = new QHBoxLayout;
+
+  auto closeButton = new QPushButton("Close");
+  connect(closeButton, &QPushButton::clicked, this, &QDialog::reject);
+
+  layout->addStretch(1);
+  layout->addWidget(closeButton);
+
+  return layout;
+}
+
+}  // namespace sup::gui
---
+++ src/lib/sup-gui-core/sup/gui/widgets/about_application_dialog.h
@@ -0,0 +1,57 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Rafael Siva (EXT)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SUP_GUI_WIDGETS_ABOUT_APPLICATION_DIALOG_H_
+#define SUP_GUI_WIDGETS_ABOUT_APPLICATION_DIALOG_H_
+
+#include <QDialog>
+#include <QLabel>
+
+namespace sup::gui
+{
+
+//! About application dialog.
+
+class AboutApplicationDialog : public QDialog
+{
+  Q_OBJECT
+public:
+  AboutApplicationDialog(QWidget *parent = nullptr, const QString &app_name = QString(),
+                         const QString &description = QString(),
+                         const QString &version = QString());
+  ~AboutApplicationDialog() override = default;
+
+  static void ShowDialog(QWidget *parent = nullptr, const QString &app_name = QString(),
+                         const QString &description = QString(),
+                         const QString &version = QString());
+
+  static QString GetCopyRight();
+
+private:
+  QLabel *CreateLinkLabel(const QString &link, const QString &name);
+
+  QLayout *CreateLogoLayout();
+  QLayout *CreateTextLayout(const QString &app_name, const QString &description,
+                            const QString &version);
+  QLayout *CreateButtonLayout();
+};
+
+}  // namespace sup::gui
+
+#endif  // SUP_GUI_WIDGETS_ABOUT_APPLICATION_DIALOG_H_
