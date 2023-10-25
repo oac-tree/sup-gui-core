484aa6673afdcf35d13199e8d045cab4d03b4153
Pospelov Gennady <Gennady.Pospelov@iter.org>
2023-10-25 11:48:09 +0200

    Add application exit codes

---
+++ src/lib/sup-gui-core/sup/gui/app/main_window_helper.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SUP_GUI_APP_MAIN_WINDOW_HELPER_H_
+#define SUP_GUI_APP_MAIN_WINDOW_HELPER_H_
+
+//! @file
+//! Collection of helper methods for main windows.
+
+namespace sup::gui
+{
+
+}  // namespace sup::gui
+
+#endif  // SUP_GUI_APP_MAIN_WINDOW_HELPER_H_
