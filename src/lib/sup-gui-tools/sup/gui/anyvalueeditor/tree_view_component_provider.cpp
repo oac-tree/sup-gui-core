d6f6499b0e197224306f13441d14a4cb89119885
Pospelov Gennady <Gennady.Pospelov@iter.org>
2023-11-08 15:17:13 +0100

    Introduce new TreeViewComponentProvider

---
+++ src/lib/sup-gui-tools/sup/gui/anyvalueeditor/tree_view_component_provider.cpp
@@ -0,0 +1,138 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "tree_view_component_provider.h"
+
+#include <sup/gui/viewmodel/anyvalue_filtered_viewmodel.h>
+#include <sup/gui/viewmodel/anyvalue_viewmodel.h>
+
+#include <mvvm/delegates/viewmodel_delegate.h>
+#include <mvvm/model/application_model.h>
+#include <mvvm/model/sessionitem.h>
+
+#include <QItemSelectionModel>
+#include <QModelIndexList>
+#include <QTreeView>
+
+namespace sup::gui
+{
+
+TreeViewComponentProvider::TreeViewComponentProvider(mvvm::ApplicationModel *model, QTreeView *view)
+    : m_view_model(std::make_unique<AnyValueViewModel>(model))
+    , m_proxy_model(std::make_unique<AnyValueFilteredViewModel>())
+    , m_delegate(std::make_unique<mvvm::ViewModelDelegate>())
+    , m_tree_view(view)
+{
+  m_proxy_model->setSourceModel(m_view_model.get());
+  m_proxy_model->setRecursiveFilteringEnabled(true);
+
+  view->setModel(m_proxy_model.get());
+  m_tree_view->setItemDelegate(m_delegate.get());
+
+  view->setModel(m_view_model.get());
+  view->setItemDelegate(m_delegate.get());
+}
+
+TreeViewComponentProvider::~TreeViewComponentProvider() = default;
+
+void TreeViewComponentProvider::SetFilterPattern(const QString &pattern)
+{
+  m_proxy_model->SetPattern(pattern);
+}
+
+const mvvm::SessionItem *TreeViewComponentProvider::GetSelectedItem() const
+{
+  auto selected = GetSelectedItems();
+  return selected.empty() ? nullptr : selected.front();
+}
+
+std::vector<const mvvm::SessionItem *> TreeViewComponentProvider::GetSelectedItems() const
+{
+  std::vector<const mvvm::SessionItem *> result;
+
+  for (auto index : GetSelectionModel()->selectedIndexes())
+  {
+    // skipping nullptr
+    if (auto item = GetItemFromViewIndex(index); item)
+    {
+      result.push_back(item);
+    }
+  }
+
+  return mvvm::utils::UniqueWithOrder(result);
+
+}
+
+void TreeViewComponentProvider::SetSelected(const mvvm::SessionItem *item)
+{
+  SetSelectedItems({item});
+}
+
+void TreeViewComponentProvider::SetSelectedItems(
+    const std::vector<const mvvm::SessionItem *> &items)
+{
+  GetSelectionModel()->clearSelection();
+  QItemSelection selection;
+  for (auto item : items)
+  {
+    for (auto index : GetViewIndices(item))
+    {
+      selection.push_back(QItemSelectionRange(index));
+    }
+  }
+
+  //  auto flags = QItemSelectionModel::Select;  // not clear, which one to use
+  auto flags = QItemSelectionModel::SelectCurrent | QItemSelectionModel::Rows;
+  GetSelectionModel()->select(selection, flags);
+}
+
+QItemSelectionModel *TreeViewComponentProvider::GetSelectionModel() const
+{
+  return m_tree_view->selectionModel();
+}
+
+const mvvm::ViewModel *TreeViewComponentProvider::GetViewModel() const
+{
+  return m_view_model.get();
+}
+
+const mvvm::SessionItem *TreeViewComponentProvider::GetItemFromViewIndex(const QModelIndex &index) const
+{
+  auto source_index = m_proxy_model ? m_proxy_model->mapToSource(index) : index;
+  return GetViewModel()->GetSessionItemFromIndex(source_index);
+}
+
+QList<QModelIndex> TreeViewComponentProvider::GetViewIndices(const mvvm::SessionItem *item) const
+{
+  const auto source_indices = GetViewModel()->GetIndexOfSessionItem(item);
+
+  if (!m_proxy_model)
+  {
+    return source_indices;
+  }
+
+  auto on_index = [this](auto index) { return m_proxy_model->mapFromSource(index); };
+  QList<QModelIndex> result;
+  std::transform(std::begin(source_indices), std::end(source_indices), std::back_inserter(result),
+                 on_index);
+
+  return result;
+}
+
+}  // namespace sup::gui
---
+++ src/lib/sup-gui-tools/sup/gui/anyvalueeditor/tree_view_component_provider.h
@@ -0,0 +1,102 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SUP_GUI_ANYVALUEEDITOR_TREE_VIEW_COMPONENT_PROVIDER_H_
+#define SUP_GUI_ANYVALUEEDITOR_TREE_VIEW_COMPONENT_PROVIDER_H_
+
+#include <QList>
+#include <QObject>
+#include <memory>
+#include <vector>
+
+class QTreeView;
+class QItemSelectionModel;
+class QModelIndex;
+
+namespace mvvm
+{
+class ApplicationModel;
+class ItemViewComponentProvider;
+class SessionItem;
+class ViewModelDelegate;
+class ViewModel;
+}  // namespace mvvm
+
+namespace sup::gui
+{
+
+class AnyValueViewModel;
+class AnyValueFilteredViewModel;
+class AnyValueItem;
+
+/**
+ * @brief The TreeViewComponentProvider class provides QAbstractItemView with custom components:
+ * viemodel, delegate and selection model.
+ *
+ * @details Provider owns components but doesn't own a view. The viewmodel allows filtering
+ * capabilities.
+ */
+
+class TreeViewComponentProvider : public QObject
+{
+  Q_OBJECT
+
+public:
+  explicit TreeViewComponentProvider(mvvm::ApplicationModel* model, QTreeView* view);
+  ~TreeViewComponentProvider() override;
+
+  void SetFilterPattern(const QString& pattern);
+
+  const mvvm::SessionItem* GetSelectedItem() const;
+  std::vector<const mvvm::SessionItem*> GetSelectedItems() const;
+
+  void SetSelected(const mvvm::SessionItem* item);
+  void SetSelectedItems(const std::vector<const mvvm::SessionItem*>& items);
+
+private:
+  QItemSelectionModel* GetSelectionModel() const;
+  const mvvm::ViewModel* GetViewModel() const;
+
+  /**
+   * @brief Returns item from view index.
+   *
+   * @details The method takes into account the existence of proxy model. When proxy model is set,
+   * the result is obtained by applying QAbstractItemProxyModel::mapToSource to given index, and
+   * then getting item out of it.
+   */
+  const mvvm::SessionItem* GetItemFromViewIndex(const QModelIndex& index) const;
+
+  /**
+   * @brief Returns view indices for given item.
+   *
+   * @details View indices can be used for operation through a view. The method takes into account
+   * the existence of proxy model. When proxy model is set, the result is obtained by applying
+   * QAbstractItemProxyModel::mapFromSource to the original item's indices.
+   */
+  QList<QModelIndex> GetViewIndices(const mvvm::SessionItem* item) const;
+
+  std::unique_ptr<AnyValueViewModel> m_view_model;
+  std::unique_ptr<AnyValueFilteredViewModel> m_proxy_model{nullptr};
+  std::unique_ptr<mvvm::ViewModelDelegate> m_delegate;
+  QTreeView* m_tree_view{nullptr};
+};
+
+}  // namespace sup::gui
+
+#endif  // SUP_GUI_ANYVALUEEDITOR_TREE_VIEW_COMPONENT_PROVIDER_H_
