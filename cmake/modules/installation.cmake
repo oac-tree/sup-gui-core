23e0706cb75880815f375cd88580c6e973abdae3
Rafael Graça Silva <rg-silva@criticalsoftware.com>
2024-02-16 13:38:45 +0000

    cmake/installation: refactor installation using PackageConfig

--- cmake/modules/installation.cmake
+++ cmake/modules/installation.cmake
@@ -4,44 +4,57 @@
 # https://gitlab.kitware.com/cmake/community/-/wikis/doc/tutorials/Exporting-and-Importing-Targets
 # -----------------------------------------------------------------------------
 
+include(PackageConfig)
+include(CMakePackageConfigHelpers)
+
+set(BUILD_CONFIGDIR ${CMAKE_BINARY_DIR})
 set(INSTALL_CONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake/sup-gui-core)
 
-# exporting targets to a script and installing it
-install(EXPORT sup-gui-core-targets FILE sup-gui-core-targets.cmake NAMESPACE sup-gui-core:: DESTINATION ${INSTALL_CONFIGDIR})
-
 # -----------------------------------------------------------------------------
-# Exporting
+# Exporting targets
 # -----------------------------------------------------------------------------
+set(PACKAGE_TARGETS_FILENAME sup-gui-core-targets.cmake)
+set(PACKAGE_TARGETS_FILE ${BUILD_CONFIGDIR}/${PACKAGE_TARGETS_FILENAME})
 
-# Add all targets to the build-tree export set
-export(TARGETS sup-gui-core sup-gui-tools NAMESPACE sup-gui-core:: FILE "${PROJECT_BINARY_DIR}/sup-gui-core-targets.cmake")
+# Install the export targets for installation usage, this does not relate to the build tree file
+install(EXPORT sup-gui-core-targets FILE ${PACKAGE_TARGETS_FILENAME} NAMESPACE sup-gui-core:: DESTINATION ${INSTALL_CONFIGDIR})
 
-# Export the package for use from the build-tree (goes to $HOME/.cmake)
+# Generate the export targets for the build tree usage
+export(TARGETS sup-gui-core sup-gui-tools NAMESPACE sup-gui-core:: FILE ${PACKAGE_TARGETS_FILE})
+
+# Export the package to CMake registry for build tree usage (goes to $HOME/.cmake)
 if(COA_EXPORT_BUILD_TREE)
   set(CMAKE_EXPORT_PACKAGE_REGISTRY ON)
   export(PACKAGE sup-gui-core)
 endif()
 
 # -----------------------------------------------------------------------------
-# Creating and installing sup-gui-core-config.cmake
+# Version configuration
 # -----------------------------------------------------------------------------
+set(PACKAGE_VERSION_FILE ${BUILD_CONFIGDIR}/sup-gui-core-config-version.cmake)
 
-include(CMakePackageConfigHelpers)
+# Generate the version config file, shared in both build tree and installation usage
+write_basic_package_version_file(${PACKAGE_VERSION_FILE} COMPATIBILITY AnyNewerVersion)
 
-# to use in the build tree
-configure_package_config_file(${SUP_GUI_CORE_PROJECT_DIR}/cmake/configs/sup-gui-core-config.cmake.in
-    ${CMAKE_CURRENT_BINARY_DIR}/sup-gui-core-config.cmake
-    INSTALL_DESTINATION ${INSTALL_CONFIGDIR}
-)
-
-# to use in install tree
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/sup-gui-core-config.cmake DESTINATION ${INSTALL_CONFIGDIR})
+install(FILES ${PACKAGE_VERSION_FILE} DESTINATION ${INSTALL_CONFIGDIR})
 
 # -----------------------------------------------------------------------------
-# Create and install sup-gui-core-config-version.cmake file
+# Package configuration
 # -----------------------------------------------------------------------------
+set(PACKAGE_CONFIG_FILE ${BUILD_CONFIGDIR}/sup-gui-core-config.cmake)
 
-write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/sup-gui-core-config-version.cmake VERSION
-    ${PROJECT_VERSION} COMPATIBILITY AnyNewerVersion)
+if(CODAC_FOUND)
+  # Set NO_CMAKE_PATH and PATHS to CMAKE_PREFIX_PATH, so that find_package will use the system Qt first
+  # if it finds one, but still look in CMAKE_PREFIX_PATH as a last resort. This gives system Qt priority over CODAC Qt
+  set(QT_FIND_OPTIONS NO_CMAKE_PATH PATHS "\$\{CMAKE_PREFIX_PATH\}")
+endif()
 
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/sup-gui-core-config-version.cmake DESTINATION ${INSTALL_CONFIGDIR})
+# Generate the package config file, shared in both build tree and installation usage
+write_package_config_file(
+  sup-gui-core
+  OUTPUT ${PACKAGE_CONFIG_FILE}
+  INSTALL_DESTINATION ${INSTALL_CONFIGDIR}
+  DEPENDENCIES Qt${QT_VERSION_MAJOR} sup-dto sup-mvvm sup-gui-extra
+  Qt${QT_VERSION_MAJOR}_FIND_OPTIONS ${QT_FIND_OPTIONS} COMPONENTS ${QT_FIND_COMPONENTS})
+
+install(FILES ${PACKAGE_CONFIG_FILE} DESTINATION ${INSTALL_CONFIGDIR})
