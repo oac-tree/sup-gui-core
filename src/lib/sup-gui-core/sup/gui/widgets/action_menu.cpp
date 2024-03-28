a617bc13fdd0699eaf4991de24025f4aa5adae2e
Pospelov Gennady <Gennady.Pospelov@iter.org>
2024-03-28 12:42:57 +0100

    Provide placeholder for ActionMenu

---
+++ src/lib/sup-gui-core/sup/gui/widgets/action_menu.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "action_menu.h"
+
+namespace sup::gui
+{
+
+ActionMenu::ActionMenu(QObject *parent) : QWidgetAction(parent)
+{
+
+}
+
+}
---
+++ src/lib/sup-gui-core/sup/gui/widgets/action_menu.h
@@ -0,0 +1,47 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SUP_GUI_WIDGETS_ACTION_MENU_H_
+#define SUP_GUI_WIDGETS_ACTION_MENU_H_
+
+#include <QWidgetAction>
+
+namespace sup::gui
+{
+
+/**
+ * @brief The ActionMenu class provides a menu with other actions intended for both taskbars and
+ * pop-up menus.
+ *
+ * When inserted in a pop-up menu, it looks like a usual action with the menu. When inserted in a
+ * toolbar, it looks like a tool button with an instant menu pop-up feature.
+ *
+ * Inspired by https://api.kde.org/frameworks/kwidgetsaddons/html/classKActionMenu.html
+ */
+class ActionMenu : public QWidgetAction
+{
+  Q_OBJECT
+
+public:
+  explicit ActionMenu(QObject* parent = nullptr);
+};
+
+}  // namespace sup::gui
+
+#endif  // SUP_GUI_WIDGETS_ACTION_MENU_H_
