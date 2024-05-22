f23d7cd71629205cde01d4a169db2caa2ccbca0a
Gennady Pospelov <gennady.pospelov@iter.org>
2024-05-22 14:46:53 +0200

    Move message_helper from sequencer-gui

---
+++ src/lib/sup-gui-core/sup/gui/components/message_helper.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "message_helper.h"
+
+namespace sup::gui
+{
+
+void SendMessage(const sup::gui::MessageEvent &event, QMessageBox::Icon icon_type)
+{
+  QMessageBox msg_box;
+  msg_box.setWindowTitle(QString::fromStdString(event.title));
+  msg_box.setText(QString::fromStdString(event.text));
+  msg_box.setInformativeText(QString::fromStdString(event.informative));
+  msg_box.setDetailedText(QString::fromStdString(event.detailed));
+  msg_box.setIcon(icon_type);
+  msg_box.exec();
+}
+
+void SendWarningMessage(const sup::gui::MessageEvent &event)
+{
+  SendMessage(event, QMessageBox::Warning);
+}
+
+void SendInfoMessage(const sup::gui::MessageEvent &event)
+{
+  SendMessage(event, QMessageBox::Information);
+}
+
+}  // namespace sup::gui
---
+++ src/lib/sup-gui-core/sup/gui/components/message_helper.h
@@ -0,0 +1,50 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SUP_GUI_COMPONENTS_MESSAGE_HELPER_H_
+#define SUP_GUI_COMPONENTS_MESSAGE_HELPER_H_
+
+//! @file
+//! Collection of helper functions to send messages.
+
+#include <sup/gui/components/message_event.h>
+
+#include <QMessageBox>
+
+namespace sup::gui
+{
+
+/**
+ * @brief Send the message via the message box.
+ */
+void SendMessage(const sup::gui::MessageEvent& event, QMessageBox::Icon icon_type);
+
+/**
+ * @brief Send warning message via the message box.
+ */
+void SendWarningMessage(const sup::gui::MessageEvent& event);
+
+/**
+ * @brief Send info message via the message box.
+ */
+void SendInfoMessage(const sup::gui::MessageEvent& event);
+
+}  // namespace sup::gui
+
+#endif  // SUP_GUI_COMPONENTS_MESSAGE_HELPER_H_
