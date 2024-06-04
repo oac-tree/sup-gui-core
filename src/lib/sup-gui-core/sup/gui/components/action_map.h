f413b68a58fe1afe756b2dee973285885e1394ef
Gennady Pospelov <gennady.pospelov@iter.org>
2024-06-04 17:51:14 +0200

    Move ActionMap from sequencer-gui to here

---
+++ src/lib/sup-gui-core/sup/gui/components/action_map.h
@@ -0,0 +1,105 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SUP_GUI_COMPONENTS_ACTION_MAP_H_
+#define SUP_GUI_COMPONENTS_ACTION_MAP_H_
+
+#include <sup/gui/core/exceptions.h>
+
+#include <QAction>
+#include <algorithm>
+#include <map>
+
+namespace sup::gui
+{
+
+/**
+ * @brief The ActionMap class is a simple map with actions stored against keys.
+ *
+ * Provides additional checks for existing keys and existing actions.
+ */
+template <typename ActionKeyT>
+class ActionMap
+{
+public:
+  void Add(ActionKeyT key, QAction* action)
+  {
+    if (auto action = GetAction(key); action)
+    {
+      throw RuntimeException("Key is already registered");
+    }
+
+    if (!action)
+    {
+      throw RuntimeException("Can't register null action");
+    }
+
+    m_action_map[key] = action;
+  }
+
+  /**
+   * @brief Returns list of actions according to provided keys.
+   */
+  QList<QAction*> GetActions(const std::vector<ActionKeyT>& action_keys) const
+  {
+    QList<QAction*> result;
+    std::transform(action_keys.begin(), action_keys.end(), std::back_inserter(result),
+                   [this](auto element) { return GetAction(element); });
+
+    return result;
+  }
+
+  /**
+   * @brief Returns action for given key.
+   */
+  QAction* GetAction(ActionKeyT key) const
+  {
+    auto iter = m_action_map.find(key);
+    return iter == m_action_map.end() ? nullptr : iter->second;
+  }
+
+  /**
+   * @brief Returns key for given action.
+   *
+   * Throws if the key can't be found.
+   */
+  ActionKeyT GetKey(QAction* action)
+  {
+    auto iter = std::find_if(m_action_map.begin(), m_action_map.end(),
+                             [action](auto element) { return element.second == action; });
+
+    if (iter == m_action_map.end())
+    {
+      throw RuntimeException("Can't convert name to RunnerStatus");
+    }
+    return iter->first;
+  }
+
+  /**
+   * @brief Returns the number of registered actions.
+   */
+  int GetSize() const { return static_cast<int>(m_action_map.size()); }
+
+private:
+  std::map<ActionKeyT, QAction*> m_action_map;
+};
+
+}  // namespace sup::gui
+
+#endif  // SUP_GUI_COMPONENTS_ACTION_MAP_H_
---
+++ test/testsup-gui-core/action_map_tests.cpp
@@ -0,0 +1,73 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "sup/gui/components/action_map.h"
+
+#include <gtest/gtest.h>
+
+using namespace sup::gui;
+
+class ActionMapTest : public ::testing::Test
+{
+public:
+  enum class Keys
+  {
+    Key1,
+    Key2,
+    Key3
+  };
+};
+
+TEST_F(ActionMapTest, AddAction)
+{
+  ActionMap<Keys> action_map;
+  EXPECT_EQ(action_map.GetSize(), 0);
+
+  QAction action1;
+  QAction action2;
+
+  // it is not possible to register null action
+  EXPECT_THROW(action_map.Add(Keys::Key1, nullptr), RuntimeException);
+
+  action_map.Add(Keys::Key1, &action1);
+  EXPECT_EQ(action_map.GetSize(), 1);
+
+  // it is not possible to register key twice
+  EXPECT_THROW(action_map.Add(Keys::Key1, &action1), RuntimeException);
+
+  action_map.Add(Keys::Key2, &action2);
+
+  EXPECT_EQ(action_map.GetActions({Keys::Key2, Keys::Key1}), QList<QAction*>({&action2, &action1}));
+}
+
+TEST_F(ActionMapTest, GetKey)
+{
+  ActionMap<Keys> action_map;
+  EXPECT_EQ(action_map.GetSize(), 0);
+
+  QAction action1;
+  QAction action2;
+
+  action_map.Add(Keys::Key1, &action1);
+  action_map.Add(Keys::Key2, &action2);
+
+  EXPECT_EQ(action_map.GetKey(&action1), Keys::Key1);
+  EXPECT_EQ(action_map.GetKey(&action2), Keys::Key2);
+  EXPECT_THROW(action_map.GetKey(nullptr), RuntimeException);
+}
