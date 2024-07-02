1be4e58f64e240a36fdd538dfcdc5bfbd6dd9159
Gennady Pospelov <gennady.pospelov@iter.org>
2024-07-02 12:53:54 +0200

    Placeholder for AnyValueWaveformEditor

---
+++ src/lib/sup-gui-tools/sup/gui/waveformeditor/anyvalue_waveform_editor.cpp
@@ -0,0 +1,52 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "anyvalue_waveform_editor.h"
+
+#include "waveform_editor.h"
+
+#include <sup/gui/model/anyvalue_item.h>
+#include <sup/gui/plotting/waveform_helper.h>
+
+#include <QVBoxLayout>
+
+namespace sup::gui
+{
+
+AnyValueWaveformEditor::AnyValueWaveformEditor(QWidget *parent)
+    : AbstractAnyValueEditor(parent), m_waveform_editor(new WaveformEditor(nullptr))
+
+{
+  auto layout = new QVBoxLayout(this);
+  layout->setContentsMargins(0, 0, 0, 0);
+  layout->setSpacing(0);
+
+  layout->addWidget(m_waveform_editor);
+}
+
+AnyValueWaveformEditor::~AnyValueWaveformEditor() = default;
+
+void AnyValueWaveformEditor::SetInitialValue(const AnyValueItem *item) {}
+
+std::unique_ptr<AnyValueItem> AnyValueWaveformEditor::GetResult()
+{
+  return {};
+}
+
+}  // namespace sup::gui
---
+++ src/lib/sup-gui-tools/sup/gui/waveformeditor/anyvalue_waveform_editor.h
@@ -0,0 +1,55 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SUP_GUI_WAVEFORMEDITOR_ANYVALUE_WAVEFORM_EDITOR_H_
+#define SUP_GUI_WAVEFORMEDITOR_ANYVALUE_WAVEFORM_EDITOR_H_
+
+#include <sup/gui/anyvalueeditor/abstract_anyvalue_editor.h>
+
+namespace sup::gui
+{
+
+class WaveformEditor;
+
+/**
+ * @brief The AnyValueWaveformEditor class is a widget with a line series canvas, a two-column
+ * table, and a toolbar for waveform editing.
+ *
+ * The initial waveform and result of waveform editing are encoded in AnyValueItem. Internally the
+ * editor operates via mvvm::LineSeriesItem machinery.
+ */
+class AnyValueWaveformEditor : public AbstractAnyValueEditor
+{
+  Q_OBJECT
+
+public:
+  explicit AnyValueWaveformEditor(QWidget* parent = nullptr);
+  ~AnyValueWaveformEditor() override;
+
+  void SetInitialValue(const sup::gui::AnyValueItem* item) override;
+
+  std::unique_ptr<sup::gui::AnyValueItem> GetResult() override;
+
+private:
+  WaveformEditor* m_waveform_editor{nullptr};
+};
+
+}  // namespace sup::gui
+
+#endif  // SUP_GUI_WAVEFORMEDITOR_ANYVALUE_WAVEFORM_EDITOR_H_
