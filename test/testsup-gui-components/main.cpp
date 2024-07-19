4a4b021e0e3bf2f11ce959fda77eccc0d5a9472b
Pospelov Gennady <Gennady.Pospelov@iter.org>
2024-07-19 10:46:53 +0200

    Introduce new library sup-gui-components, and move "viewmodel" folder to it

---
+++ test/testsup-gui-components/main.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include <sup/gui/components/custom_metatypes.h>
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  sup::gui::RegisterCustomMetaTypes();
+
+  // run all google tests
+  return RUN_ALL_TESTS();
+}
