fb0e54b8b5aa96dc4a01e3ef93146804d1b7fd61
Gennady Pospelov <gennady.pospelov@iter.org>
2024-10-17 16:32:52 +0200

    Implement IProjectHandler interface and new version of ProjectHandlerV2

---
+++ src/lib/sup-gui-views/sup/gui/project/project_handler_v2.cpp
@@ -0,0 +1,157 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "project_handler_v2.h"
+
+#include "file_based_user_interactor.h"
+#include "folder_based_user_interactor.h"
+#include "recent_project_settings.h"
+
+#include <sup/gui/core/exceptions.h>
+
+#include <mvvm/project/i_project.h>
+#include <mvvm/project/project_context.h>
+#include <mvvm/project/project_manager_factory.h>
+#include <mvvm/project/project_utils.h>
+#include <mvvm/widgets/widget_utils.h>
+
+#include <QMainWindow>
+
+namespace sup::gui
+{
+
+namespace
+{
+
+/**
+ * @brief Returns user interactor corresponding to the given project.
+ *
+ * It will provide dialogs to open existing projects, or create new.
+ */
+std::unique_ptr<sup::gui::AbstractProjectUserInteractor> CreateUserInteractor(
+    mvvm::IProject *project)
+{
+  auto application_type = QString::fromStdString(project->GetApplicationType());
+
+  if (project->GetProjectType() == mvvm::ProjectType::kFolderBased)
+  {
+    return std::make_unique<sup::gui::FolderBasedUserInteractor>(application_type,
+                                                                 mvvm::utils::FindMainWindow());
+  }
+
+  if (project->GetProjectType() == mvvm::ProjectType::kFileBased)
+  {
+    return std::make_unique<sup::gui::FileBasedUserInteractor>(application_type,
+                                                               mvvm::utils::FindMainWindow());
+  }
+
+  throw RuntimeException("Unknown project type");
+}
+
+}  // namespace
+
+ProjectHandlerV2::ProjectHandlerV2(mvvm::IProject *project)
+    : m_user_interactor(CreateUserInteractor(project))
+    , m_recent_projects(std::make_unique<RecentProjectSettings>())
+    , m_project_manager(CreateProjectManager(*project, m_user_interactor->CreateContext()))
+{
+  UpdateNames();
+}
+
+ProjectHandlerV2::~ProjectHandlerV2() = default;
+
+bool ProjectHandlerV2::CloseCurrentProject()
+{
+  auto result = m_project_manager->CloseCurrentProject();
+  if (result)
+  {
+    UpdateNames();
+  }
+  return result;
+}
+
+void ProjectHandlerV2::CreateNewProject()
+{
+  if (m_project_manager->CreateNewProject({}))
+  {
+    UpdateNames();
+  }
+}
+
+void ProjectHandlerV2::OpenExistingProject(const QString &path)
+{
+  if (m_project_manager->OpenExistingProject(path.toStdString()))
+  {
+    UpdateNames();
+  }
+}
+
+void ProjectHandlerV2::SaveCurrentProject()
+{
+  if (m_project_manager->SaveCurrentProject())
+  {
+    UpdateNames();
+  }
+}
+
+void ProjectHandlerV2::SaveProjectAs()
+{
+  if (m_project_manager->SaveProjectAs({}))
+  {
+    UpdateNames();
+  }
+}
+
+void ProjectHandlerV2::ClearRecentProjectsList()
+{
+  m_recent_projects->ClearRecentProjectsList();
+  UpdateNames();
+}
+
+QStringList ProjectHandlerV2::GetRecentProjectList() const
+{
+  return m_recent_projects->GetRecentProjectList();
+}
+
+void ProjectHandlerV2::UpdateNames()
+{
+  UpdateCurrentProjectName();
+  UpdateRecentProjectNames();
+}
+
+void ProjectHandlerV2::UpdateCurrentProjectName()
+{
+  const auto current_project_dir = m_project_manager->CurrentProjectPath();
+  const auto is_modified = m_project_manager->IsModified();
+
+  // set main window title
+  auto title = mvvm::utils::ProjectWindowTitle(current_project_dir, is_modified);
+  if (auto main_window = mvvm::utils::FindMainWindow(); main_window)
+  {
+    main_window->setWindowTitle(QString::fromStdString(title));
+  }
+}
+
+void ProjectHandlerV2::UpdateRecentProjectNames()
+{
+  m_recent_projects->AddToRecentProjectList(
+      QString::fromStdString(m_project_manager->CurrentProjectPath()));
+}
+
+}  // namespace sup::gui
---
+++ src/lib/sup-gui-views/sup/gui/project/project_handler_v2.h
@@ -0,0 +1,91 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SUP_GUI_PROJECT_PROJECT_HANDLER_V2_H_
+#define SUP_GUI_PROJECT_PROJECT_HANDLER_V2_H_
+
+#include <sup/gui/project/i_project_handler.h>
+
+#include <memory>
+
+namespace mvvm
+{
+class IProject;
+class IProjectManager;
+}  // namespace mvvm
+
+namespace sup::gui
+{
+
+class AbstractProjectUserInteractor;
+class RecentProjectSettings;
+
+class ProjectHandlerV2 : public IProjectHandler
+{
+public:
+  explicit ProjectHandlerV2(mvvm::IProject* project);
+
+  ~ProjectHandlerV2() override;
+
+  bool CloseCurrentProject() override;
+
+  void CreateNewProject() override;
+
+  void OpenExistingProject(const QString& path) override;
+
+  void SaveCurrentProject() override;
+
+  void SaveProjectAs() override;
+
+  void ClearRecentProjectsList() override;
+
+  QStringList GetRecentProjectList() const override;
+
+private:
+  /**
+   * @brief Performs internal updates related to project name change.
+   */
+  void UpdateNames();
+
+  /**
+   * @brief Updates the name of the current project on main window.
+   */
+  void UpdateCurrentProjectName();
+
+  /**
+   * @brief Updates recent project list in settings.
+   */
+  void UpdateRecentProjectNames();
+
+  //!< main project
+  mvvm::IProject* m_project_agent{nullptr};
+
+  //!< knows how to interact with the user
+  std::unique_ptr<AbstractProjectUserInteractor> m_user_interactor;
+
+  //!< performs actual model saving
+  std::unique_ptr<mvvm::IProjectManager> m_project_manager;
+
+  //!< tracks recent projects
+  std::unique_ptr<sup::gui::RecentProjectSettings> m_recent_projects;
+};
+
+}  // namespace sup::gui
+
+#endif  // SUP_GUI_PROJECT_PROJECT_HANDLER_V2_H_
