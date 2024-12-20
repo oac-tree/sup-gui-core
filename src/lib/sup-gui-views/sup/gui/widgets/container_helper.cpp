ab7f40a88842b7b5da2a6ba4809f74256f24a35d
Pospelov Gennady <Gennady.Pospelov@iter.org>
2024-12-20 17:10:50 +0100

    Create helper methods to convert list to byte array

---
+++ src/lib/sup-gui-views/sup/gui/widgets/container_helper.cpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "container_helper.h"
+
+#include <QIODevice>
+#include <QByteArray>
+#include <QDataStream>
+
+namespace sup::gui
+{
+
+QByteArray GetArray(const QList<int> &list)
+{
+  QByteArray data;
+  QDataStream stream(&data, QIODevice::WriteOnly);
+  stream << list;
+  return data;
+}
+
+QList<int> GetList(const QByteArray &array)
+{
+  QByteArray sd = array;
+  QDataStream stream(&sd, QIODevice::ReadOnly);
+  QList<int> result;
+  stream >> result;
+  return result;
+}
+
+
+}
---
+++ test/testsup-gui-views/container_helper_tests.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP and PSPS
+ *
+ * Description   : Common libraries and tools for Operation Application GUIs
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "sup/gui/widgets/container_helper.h"
+
+#include <gtest/gtest.h>
+
+namespace sup::gui
+{
+
+class ContainerHelperTest : public ::testing::Test
+{
+};
+
+TEST_F(ContainerHelperTest, ListToByteArray)
+{
+  QList<int> list({1, 2, 3});
+  auto byte_array = GetArray(list);
+
+  EXPECT_EQ(GetList(byte_array), list);
+}
+}  // namespace sup::gui
